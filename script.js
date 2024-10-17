3afd61df16dc45dd4bff7d17a40ee06b56c9e43a
ArticVl <148974718+ArticVl@users.noreply.github.com>
2024-10-17 12:02:06 -0300

    site melhor

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+    const relatoButton = document.getElementById('relatoButton');
+    const relatoSection = document.getElementById('relatos');
+    const dataSection = document.getElementById('datas');
+    
+    relatoButton.addEventListener('click', function() {
+        if (relatoSection.style.display === 'none') {
+            relatoSection.style.display = 'block';
+            dataSection.style.display = 'block';
+        } else {
+            relatoSection.style.display = 'none';
+            dataSection.style.display = 'none';
+        }
+    });
+
+    // Inicialmente escondendo as seções
+    relatoSection.style.display = 'none';
+    dataSection.style.display = 'none';
+    
     const menuItems = document.querySelectorAll('nav ul li');
 
     menuItems.forEach(item => {
